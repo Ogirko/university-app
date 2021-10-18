611e29cfb2c487f320729492e8621027b70600d8
Roman Didushok <roma.didushok@gmail.com>
2021-10-18 22:47:22 +0300

    Uploading code

---
+++ frontend/src/data.js
@@ -0,0 +1,72 @@
+const data = {
+    products : [
+        {
+            _id: '1',
+            name: 'Nike Slim Shirt',
+            category: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 120,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality product'
+        },
+        {
+            _id: '2',
+            name: 'Nike Regular Shirt',
+            category: 'Shirts',
+            image: '/images/p2.jpg',
+            price: 100,
+            brand: 'Nike',
+            rating: 4.8,
+            numReviews: 10,
+            description: 'High quality product'
+        },
+        {
+            _id: '3',
+            name: 'Reebok Slim Shirt',
+            category: 'Shirts',
+            image: '/images/p3.jpg',
+            price: 90,
+            brand: 'Reebok',
+            rating: 4.2,
+            numReviews: 14,
+            description: 'High quality product'
+        },
+        {
+            _id: '4',
+            name: 'Levis Slim Pants',
+            category: 'Pants',
+            image: '/images/p4.jpg',
+            price: 130,
+            brand: 'Levis',
+            rating: 4.9,
+            numReviews: 17,
+            description: 'High quality product'
+        },
+        {
+            _id: '5',
+            name: 'Nike Free Shirt',
+            category: 'Shirts',
+            image: '/images/p5.jpg',
+            price: 120,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 27,
+            description: 'High quality product'
+        },
+        {
+            _id: '6',
+            name: 'Adidas Slim Pants',
+            category: 'Pants',
+            image: '/images/p6.jpg',
+            price: 170,
+            brand: 'Adidas',
+            rating: 4.5,
+            numReviews: 16,
+            description: 'High quality product'
+        },
+    ],
+};
+
+export default data;
